7228497f802b601f3e4e014b502cbecd63b685f5
espurrell <150044207+espurrell@users.noreply.github.com>
2024-06-04 11:45:00 -0230

    second commit

--- Student.java
+++ Student.java
@@ -4,9 +4,9 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
-    private String courses;
-    private int tuitionBalance;
+    private String studentID;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600; // static - not specific to object
     private static int id = 1001;
 
@@ -21,13 +21,42 @@ public class Student {
 
         System.out.print("1-Freshman\n2-Sophmore\n3-Junior\n4-Senior\n Enter student class level: ");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName + " " + lastName + " " + gradeYear);
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
 
     }
 
     // generate an ID
+    private void setStudentID() {
+        // gradelevel + ID
+        id++; // increment id each time one is created
+        this.studentID = gradeYear + "" + id;
+
+    }
 
     // enroll in courses
+
+    public void enroll() {
+        // get inside a loop, user hits 0 when done enrolling
+        do {
+            System.out.print("Enter course to enroll, Q to quit: ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+
+            else {
+                break;
+            }
+        } while (1 != 0);
+
+        System.out.println("ENROLLED IN:" + courses);
+        System.out.println("TUITION BALANCE: " + tuitionBalance);
+    }
 
     // view balance
 
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -2,7 +2,7 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
         Student stu1 = new Student();
-
+        stu1.enroll();
         // ask how many new users we want to add
 
         // create n number of new students
